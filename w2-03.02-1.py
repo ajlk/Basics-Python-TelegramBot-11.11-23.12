4dd5c933e546fc9700d75de5e656a771a0a7d4ca
ajlk <ajlk05@gmail.com>
2019-11-18 16:08:16 +0100

    Notes added

---
+++ w2-03.02-1.py
@@ -0,0 +1,8 @@
+# The function returns a value depending on x
+def f(x):
+    if x <= -2:
+        return 1 - (x + 2) ** 2
+    elif -2 < x <= 2:
+        return -x / 2
+    else:
+        return (x - 2) ** 2 + 1
