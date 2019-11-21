5e8c6e4b41834c81d79ba7b82e0d605a7e0db06d
ajlk <ajlk05@gmail.com>
2019-11-21 18:24:15 +0100

    Бот. Начало.

---
+++ w2_bot.py
@@ -0,0 +1,37 @@
+import telebot
+
+token = "1018644560:AAHXrkVLAnl8DeVJT6x0WrLyhuGRbWU4QaA"
+
+bot = telebot.TeleBot(token)
+
+
+@bot.message_handler(func=lambda message: True)
+def weather(message):
+    if message.text == "/start":
+        bot.send_message(
+            message.chat.id,
+            "Это бот-погода. Поможет узнать погоду в любом городе. Какой город интересует?"
+        )
+    elif message.text == "Москва":
+        bot.send_message(
+            message.chat.id,
+            "Сейчас отличная погода!"
+        )
+    elif message.text == "Москва завтра":
+        bot.send_message(
+            message.chat.id,
+            "Завтра еще лучше!"
+        )
+    elif message.text == "Мордор":                      # Пасхалка
+        bot.send_message(
+            message.chat.id,
+            "В Мордоре всегда збс. Ждём в гости с печеньками и винишком!"
+        )
+    else:
+        bot.send_message(
+            message.chat.id,
+            "Я тебя не понял"
+        )
+
+
+bot.polling()
