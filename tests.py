21612623234ad7206bd28a68167ceff70c682f16
ajlk <ajlk05@gmail.com>
2019-12-02 18:38:21 +0100

    cleaning

--- tests.py
+++ tests.py
@@ -1,13 +1,3 @@
-matrix = [[1, 2, 3], [4, 5, 6]]
-matrix_new = matrix.copy()
+TEXT = "Мама мыла раму, и ещё какой-то. текст.с точками и пробелами"
 
-print(matrix_new)
-matrix[0][0] = 10
-print(matrix_new)
-
-"""
-matrix_new = matrix[:]
-# 
-# matrix_new = matrix * 1
-matrix_new = [*matrix]
-"""
+lext_2= TEXT.sp
