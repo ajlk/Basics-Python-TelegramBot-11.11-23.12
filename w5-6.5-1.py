e5263aba48422d12741e615ce88cff880636de2a
ajlk05 <ajlk05@gmail.com>
2019-12-14 16:52:44 +0100

    ...

--- w5-6.5-1.py
+++ w5-6.5-1.py
@@ -1,24 +1,36 @@
 class multifilter:
     def judge_half(pos, neg):
-        return pos>= neg
+        return pos >= neg
         # допускает элемент, если его допускает хотя бы половина фукнций (pos >= neg)
 
     def judge_any(pos, neg):
-        return pos>=1
+        return pos >= 1
         # допускает элемент, если его допускает хотя бы одна функция (pos >= 1)
 
     def judge_all(pos, neg):
-        return neg==0
+        return neg == 0
         # допускает элемент, если его допускают все функции (neg == 0)
 
     def __init__(self, iterable, *funcs, judge=judge_any):
-        # iterable - исходная последовательность
-        # funcs - допускающие функции
-        # judge - решающая функция
+        self.iterable = iterable
+        self.funcs = funcs
+        self.judge = judge
+
+    # iterable - исходная последовательность
+    # funcs - допускающие функции
+    # judge - решающая функция
 
     def __iter__(self):
-        # возвращает итератор по результирующей последовательности
+        for i in self.iterable:
+            pos = 0
+            neg = 0
 
+            for func in self.funcs:
+                if func(i):
+                    pos += 1
+                else:
+                    neg += 1
 
-a=[i for i in range(31)]
-print(list(multifilter(a, mul2, mul3, mul5)))
+            if self.judge(pos, neg):
+                yield i
+    # возвращает итератор по результирующей последовательности
---
+++ w5-6.5-2.py
@@ -0,0 +1,9 @@
+class prime:
+    def __init__(self):
+        pass
+
+
+
+
+print(list(itertools.takewhile(lambda x : x <= 31, primes())))
+# [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31]
