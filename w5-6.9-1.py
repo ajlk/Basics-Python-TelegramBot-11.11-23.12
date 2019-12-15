351049bc1b2dae2bbc2cc1c95e886355f541c0ae
ajlk05 <ajlk05@gmail.com>
2019-12-15 17:19:17 +0100

    ...

--- w5-6.9-1.py
+++ w5-6.9-1.py
@@ -5,11 +5,10 @@ api_url = 'http://numbersapi.com/'
 with open('dataset_263163_3.txt') as inp_data:
     for line in inp_data:
         line = line.strip()
-        res = requests.get(f'{api_url}/{line}/math?json=true')
-        dict = res.json()
-        for key, value in dict.items():
+        res = (requests.get(f'{api_url}/{line}/math?json=true')).json()
+        for key, value in res.items():
             if key == 'found':
-                if dict[key]:
+                if res[key]:
                     print('Interesting')
                 else:
                     print('Boring')
